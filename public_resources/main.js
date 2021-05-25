4c8a97a4c040d5e0b9c9b52d6bcb0694ac43ad50
PecceG2 <peccettog@live.com>
2021-05-25 18:22:56 -0300

    + Soporte para múltiples reproductores + Configuración vía tag HTML * Fix diseño

--- public_resources/main.js
+++ public_resources/main.js
@@ -1,48 +1,126 @@
 $(function(){
-    $(document).on('click', '.icon', function(e){
-        let isPlaying = false;
-        if($(this).hasClass("icon-pause")){
-            isPlaying = true;
+    // Ready
+
+    $(".line-player .main-frame .configurator").each(function(){
+        let player = $(this).parent().parent();
+        let playerConfiguration = {
+            title : $(this).data("title"),
+            subtitle : $(this).data("subtitle"),
+            imgIcon : $(this).data("imgicon"),
+            streamType : $(this).data("streamtype"),
+            streamFormat : $(this).data("streamformat"),
+            streamURL : $(this).data("streamurl"),
+            volume : $(this).data("volume"),
+            autoPlay : $(this).data("autoplay"),
+            backgroundColor : hexToRgb($(this).data("backgroundcolor")),
+            fontColor : hexToRgb($(this).data("fontcolor"))
+        };
+
+
+        // Set styles
+        player.find(".main-frame .main-container .infobox h2").text(playerConfiguration.title);
+        player.find(".main-frame .main-container .infobox h4").text(playerConfiguration.subtitle);
+        player.find(".img-album").css("background-image", 'url("'+playerConfiguration.imgIcon+'")');
+        player.find(".main-frame").css("border-color", "rgba("+playerConfiguration.backgroundColor.r+", "+playerConfiguration.backgroundColor.g+", "+playerConfiguration.backgroundColor.b+", 0.5)");
+        player.find(".main-frame").css("background-color", "rgba("+playerConfiguration.backgroundColor.r+", "+playerConfiguration.backgroundColor.g+", "+playerConfiguration.backgroundColor.b+", 0.5)");
+        player.find(".main-frame .volume-color-print").css("background-color", "rgba("+playerConfiguration.backgroundColor.r+", "+playerConfiguration.backgroundColor.g+", "+playerConfiguration.backgroundColor.b+", 0.2)");
+
+
+        // Set player functions
+        let validPlayer = true;
+        switch(playerConfiguration.streamType){
+            case "audio":
+                switch(playerConfiguration.streamFormat){
+                    case "shoutcast":
+                    case "icecast":
+                    case "shoutcast2":
+                    case "MP3":
+                        player.find(".player").html("<audio class='playerObject'><source src='"+playerConfiguration.streamURL+"' type='audio/mpeg'/></audio>");
+                        player.find(".playerObject").attr("src", playerConfiguration.streamURL);
+                        player.find(".playerObject")[0].load();
+                        break;
+                    case "M3U8":
+                    case "HLS":
+                    case "DASH":
+                        player.find(".player").html("<video class='playerObject'></video>");
+                        let playerJSObj = player.find(".player .playerObject")[0];
+                        if(Hls.isSupported()){
+                            let hls = new Hls();
+                            hls.loadSource(playerConfiguration.streamURL);
+                            hls.attachMedia(playerJSObj);
+                        }else if(playerJSObj.canPlayType('application/vnd.apple.mpegurl')){
+                            playerJSObj.src = playerConfiguration.streamURL;
+                        }
+                        break;
+                    default:
+                        validPlayer = false;
+                        console.error("Line Player Error: Invalid player 'streamFormat', valid options: [shoutcast | shoutcast2 | icecast | MP3 | M3U8 | HLS | DASH]");
+                }
+                break;
+            case "video":
+                break;
+            default:
+                validPlayer = false;
+                console.error("Line Player Error: Invalid player 'streamType', valid options: [audio | video]");
         }
 
-        if(isPlaying){
-            $(this).removeClass("icon-pause").addClass("icon-play");
 
-        }else{
-            $(this).removeClass("icon-play").addClass("icon-pause");
+        if(validPlayer){
+            changeVolume(player, playerConfiguration.volume);
+            if(playerConfiguration.autoPlay){
+                togglePlayPause(player.find(".main-button .icon"), true);
+            }
         }
-
-
-
-
-        let video = document.getElementsByClassName('player')[0];
-        var videoSrc = 'https://mega.stweb.tv/mega983/live/chunks.m3u8';
-        if (Hls.isSupported()) {
-            var hls = new Hls();
-            hls.loadSource(videoSrc);
-            hls.attachMedia(video);
-        }
-            // HLS.js is not supported on platforms that do not have Media Source
-            // Extensions (MSE) enabled.
-            //
-            // When the browser has built-in HLS support (check using `canPlayType`),
-            // we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video
-            // element through the `src` property. This is using the built-in support
-            // of the plain video element, without using HLS.js.
-            //
-            // Note: it would be more normal to wait on the 'canplay' event below however
-            // on Safari (where you are most likely to find built-in HLS support) the
-            // video.src URL must be on the user-driven white-list before a 'canplay'
-            // event will be emitted; the last video event that can be reliably
-        // listened-for when the URL is not on the white-list is 'loadedmetadata'.
-        else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-            video.src = videoSrc;
-        }
-
-
     });
 
 
+
+
+
+
+    $(document).on('click', '.icon', function(e){
+        togglePlayPause($(this));
+    });
+
+    function togglePlayPause(iconPlayPauseObj, withoutUserInteraction = false){
+        let isPlaying = false;
+        let playerObj = iconPlayPauseObj.parent().parent().find(".playerObject");
+        if(iconPlayPauseObj.hasClass("icon-pause")){
+            isPlaying = true;
+        }
+
+
+        if(withoutUserInteraction){
+            let JSHTML5Player = playerObj.get(0);
+            JSHTML5Player.play();
+            if(JSHTML5Player.paused){
+                playerObj.trigger('play');
+            }
+            if(!JSHTML5Player.paused){
+                // Without user interact trigger worked, change icon status.
+                iconPlayPauseObj.removeClass("icon-play").addClass("icon-pause");
+            }
+
+            return;
+        }
+
+        if(isPlaying){
+            iconPlayPauseObj.removeClass("icon-pause").addClass("icon-play");
+            playerObj.trigger('pause');
+        }else{
+            iconPlayPauseObj.removeClass("icon-play").addClass("icon-pause");
+            playerObj.trigger('play');
+        }
+    }
+
+
+
+
+    /*
+
+        VOLUME SETTINGS
+
+     */
     $('.volume-overlay').on('mousedown', function() {
         $('.volume-overlay').on('mousemove', function(e) {
             $('html,body').stop(false, true);
@@ -50,32 +128,7 @@ $(function(){
             let longX = grabber.width();
             let xCursor = e.pageX - grabber.offset().left;
             let percentX = (xCursor*100)/longX;
-
-            if(percentX >= 100){
-                percentX = 100;
-            }
-
-            $(".volume-color-print").width(percentX+"%");
-            $(".volume h5").text(Math.trunc(percentX)+"%");
-            $(".volume i").removeClass('fa-volume-off').removeClass('fa-volume-down').removeClass('fa-volume-up');
-
-            if((percentX/100) <= 0){
-                $(".player").prop("volume", 0);
-            }else{
-                $(".player").prop("volume", percentX/100);
-            }
-
-
-            if(percentX > 60){
-                $(".volume i").addClass('fa-volume-up');
-            }else if(percentX > 0){
-                $(".volume i").addClass('fa-volume-down');
-            }else{
-                $(".volume i").addClass('fa-volume-off');
-            }
-
-
-            //console.log('x: '+Math.trunc(percentX)+" | y: "+percentY);
+            changeVolume($(this), percentX);
         });
 
         $('.volume-overlay').on('mouseleave', function(e){
@@ -86,5 +139,54 @@ $(function(){
     $('.volume-overlay').on('mouseup', function() {
         $('.volume-overlay').off('mousemove');
     });
+
+
+    function changeVolume(playerJQObj, volumePercent){
+        if(volumePercent >= 100){
+            volumePercent = 100;
+        }
+        if(volumePercent <= 0){
+            volumePercent = 0;
+        }
+
+        playerJQObj.find(".volume-color-print").width(volumePercent+"%");
+        playerJQObj.parent().find(".volume h5").text(Math.trunc(volumePercent)+"%");
+        playerJQObj.parent().find(".volume i").removeClass('fa-volume-off').removeClass('fa-volume-down').removeClass('fa-volume-up');
+
+        if((volumePercent/100) <= 0){
+            playerJQObj.parent().find(".playerObject").prop("volume", 0);
+        }else{
+            playerJQObj.parent().find(".playerObject").prop("volume", volumePercent/100);
+        }
+
+        if(volumePercent > 60){
+            playerJQObj.parent().find(".volume i").addClass('fa-volume-up');
+        }else if(volumePercent > 0){
+            playerJQObj.parent().find(".volume i").addClass('fa-volume-down');
+        }else{
+            playerJQObj.parent().find(".volume i").addClass('fa-volume-off');
+        }
+    }
+
+
+
+    /*
+
+        OTHER INTERNAL FUNCTIONS
+
+     */
+    function hexToRgb(hex) {
+        let shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+        hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+            return r + r + g + g + b + b;
+        });
+
+        let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    }
 
 });
